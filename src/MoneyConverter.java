90003cbdc05f26ecd555e6c89971f16dcac129ee
Solodyankin Konstantin <Solodyankin.k@gmail.com>
2022-06-05 15:39:44 +0500

    work program v0.1

--- src/MoneyConverter.java
+++ src/MoneyConverter.java
@@ -1,5 +1,25 @@
+/* Напишите программу, которая конвертирует сумму денег из
+китайских юаней в российские рубли по курсу покупки 101.98. */
+import java.util.Scanner;
+
 public class MoneyConverter {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        double yuan; // сумма денег в китайских юаней
+        double rouble; // сумма денег в российских рублях
+        final double ROUBLES_PER_YUAN = 10.19; // курс покупки юаня за 1 шт.
+
+        Scanner input = new Scanner(System.in);
+
+        // Вводные данные
+        // Сумма в юанях для конвертации
+        System.out.print("Введите количество юаней для конвертации в рубли: ");
+        yuan = input.nextDouble();
+
+        // Расчёт
+        rouble = ROUBLES_PER_YUAN * yuan;
+
+        // Выводимые данные
+        // Результат конвертации в рублях
+        System.out.println("Результат конвертации: " + (int)(rouble * 100) / 100.0 + " российских рублей.");
     }
 }
